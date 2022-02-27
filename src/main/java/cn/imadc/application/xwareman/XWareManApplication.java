df524e13f7cadd7e22a9fe0df02497151ca1023d
杜劲松 <1396787304@qq.com>
2022-02-27 22:16:31 +0800

    init

---
+++ src/main/java/cn/imadc/application/xwareman/XWareManApplication.java
@@ -0,0 +1,13 @@
+package cn.imadc.application.xwareman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XWareManApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XWareManApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/imadc/application/xwareman/XWareManApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.imadc.application.xwareman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XWareManApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
