64ec666c1a94d00f34bb48aa3a019a643270938b
杜劲松 <1396787304@qq.com>
2022-03-04 00:01:57 +0800

    redis通讯

--- src/main/java/cn/imadc/application/xwareman/XWareManInstance.java
+++ src/main/java/cn/imadc/application/xwareman/XWareManInstance.java
@@ -1,13 +1,12 @@
 package cn.imadc.application.xwareman;
 
-import cn.imadc.application.xwareman.base.config.NettyClientConfig;
-import org.apache.rocketmq.common.protocol.body.TopicConfigSerializeWrapper;
-import org.apache.rocketmq.common.protocol.header.namesrv.QueryDataVersionRequestHeader;
-import org.apache.rocketmq.remoting.exception.RemotingConnectException;
-import org.apache.rocketmq.remoting.exception.RemotingSendRequestException;
-import org.apache.rocketmq.remoting.exception.RemotingTimeoutException;
-import org.apache.rocketmq.remoting.exception.RemotingTooMuchRequestException;
-import org.apache.rocketmq.remoting.protocol.RemotingCommand;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+import io.lettuce.core.sentinel.api.StatefulRedisSentinelConnection;
+import io.lettuce.core.sentinel.api.sync.RedisSentinelCommands;
+
+import java.time.Duration;
 
 /**
  * <p>
@@ -19,26 +18,39 @@ import org.apache.rocketmq.remoting.protocol.RemotingCommand;
  */
 public class XWareManInstance {
 
-    public static void main(String[] args) throws RemotingConnectException, RemotingSendRequestException
-            , RemotingTimeoutException, InterruptedException, RemotingTooMuchRequestException {
-        NettyClientConfig nettyClientConfig = new NettyClientConfig();
-        RocketMQNettyRemotingClient remotingClient = new RocketMQNettyRemotingClient(nettyClientConfig
-                , null);
-        remotingClient.start();
-        QueryDataVersionRequestHeader requestHeader = new QueryDataVersionRequestHeader();
-        requestHeader.setBrokerAddr("10.100.0.190:30921");
-        requestHeader.setBrokerId(0L);
-        requestHeader.setBrokerName("RaftNode00");
-        requestHeader.setClusterName("RaftCluster");
-        RemotingCommand remotingCommand = RemotingCommand.createRequestCommand(322, requestHeader);
+//    public static void main(String[] args) throws RemotingConnectException, RemotingSendRequestException
+//            , RemotingTimeoutException, InterruptedException, RemotingTooMuchRequestException {
+//        NettyClientConfig nettyClientConfig = new NettyClientConfig();
+//        RocketMQNettyRemotingClient remotingClient = new RocketMQNettyRemotingClient(nettyClientConfig
+//                , null);
+//        remotingClient.start();
+//        QueryDataVersionRequestHeader requestHeader = new QueryDataVersionRequestHeader();
+//        requestHeader.setBrokerAddr("10.100.0.190:30921");
+//        requestHeader.setBrokerId(0L);
+//        requestHeader.setBrokerName("RaftNode00");
+//        requestHeader.setClusterName("RaftCluster");
+//        RemotingCommand remotingCommand = RemotingCommand.createRequestCommand(322, requestHeader);
+//
+//        TopicConfigSerializeWrapper topicConfigWrapper = new TopicConfigSerializeWrapper();
+//        remotingCommand.setBody(topicConfigWrapper.getDataVersion().encode());
+//
+//        RemotingCommand response = remotingClient.invokeSync("127.0.0.1:9876", remotingCommand, 3000);
+//        System.out.println(response);
+//
+//        remotingClient.shutdown();
+//    }
 
-        TopicConfigSerializeWrapper topicConfigWrapper = new TopicConfigSerializeWrapper();
-        remotingCommand.setBody(topicConfigWrapper.getDataVersion().encode());
+    public static void main(String[] args) {
+        RedisClient redisClient = RedisClient.create("redis://192.168.137.200:6379");
+        redisClient.setDefaultTimeout(Duration.ofMillis(10 * 1000));
+        StatefulRedisConnection<String, String> nodeConnection = redisClient.connect();
+        RedisCommands<String, String> nodeCommands = nodeConnection.sync();
 
-        RemotingCommand response = remotingClient.invokeSync("127.0.0.1:9876", remotingCommand, 3000);
-        System.out.println(response);
+        String info = nodeCommands.info();
 
-        remotingClient.shutdown();
+        StatefulRedisSentinelConnection<String, String> sentinelConnection = redisClient.connectSentinel();
+
+        RedisSentinelCommands<String, String> sentinelCommands = sentinelConnection.sync();
+        info = sentinelCommands.info();
     }
-
 }
